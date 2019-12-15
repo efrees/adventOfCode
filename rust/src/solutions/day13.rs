d1ff9e987bc83e4787b59e5df152e9512dbc6e4b
Eric Frees <efrees@atlasrfid.com>
2019-12-14 18:39:31 -0600

    Support manual controls for game in day 13

--- rust/src/solutions/day13.rs
+++ rust/src/solutions/day13.rs
@@ -1,4 +1,5 @@
 use adventlib::grid::*;
+use crossterm::event::{poll, read, Event, KeyCode};
 use crossterm::{cursor, execute, style, terminal, Result};
 use intcode::*;
 use std::io::{stdout, Write};
@@ -7,6 +8,7 @@ use std::thread;
 use std::time::Duration;
 
 static RENDER_GAME_FRAMES: bool = false;
+static ENABLE_MANUAL_CONTROL: bool = false;
 
 pub fn solve() {
     println!("Day 13");
@@ -21,12 +23,14 @@ pub fn solve() {
     screen_state.print(&render_tile);
     println!("Number of block tiles (part 1): {}", block_count);
 
-    let mut screen_state = SparseGrid::<u8>::new();
-    let mut program_state = parse_program(&raw_program);
-    program_state[0] = 2;
-    let score = run_arcade_game(&mut screen_state, program_state).expect("Game errored");
+    loop {
+        let mut screen_state = SparseGrid::<u8>::new();
+        let mut program_state = parse_program(&raw_program);
+        program_state[0] = 2;
+        let score = run_arcade_game(&mut screen_state, program_state).expect("Game errored");
 
-    println!("Final score (part 2): {}", score);
+        println!("Final score (part 2): {}", score);
+    }
 }
 
 fn run_arcade_game(screen: &mut SparseGrid<u8>, program_state: Vec<i64>) -> Result<i64> {
@@ -72,6 +76,9 @@ fn run_arcade_game(screen: &mut SparseGrid<u8>, program_state: Vec<i64>) -> Resu
 }
 
 fn decide_move_direction(screen: &SparseGrid<u8>, previous_ball_pos: Option<Point>) -> i64 {
+    if ENABLE_MANUAL_CONTROL {
+        return read_direction_from_keyboard().unwrap();
+    }
     let ball_position = find_ball(screen).expect("There should be a ball");
     let paddle_position = screen
         .iter()
@@ -127,7 +134,7 @@ fn close_game_window() -> Result<()> {
 }
 
 fn render_frame(frame: &SparseGrid<u8>, score: i64) -> Result<()> {
-    thread::sleep(Duration::from_millis(30));
+    thread::sleep(Duration::from_millis(300));
 
     let rendered_screen = frame.render_to_string(&render_tile);
     execute!(
@@ -137,4 +144,24 @@ fn render_frame(frame: &SparseGrid<u8>, score: i64) -> Result<()> {
         cursor::RestorePosition,
         style::Print(format!("{}", score)),
     )
+}
+
+fn read_direction_from_keyboard() -> Result<i64> {
+    clear_event_stream()?;
+    match read()? {
+        Event::Key(event) => match event.code {
+            KeyCode::Left => Ok(-1),
+            KeyCode::Right => Ok(1),
+            _ => Ok(0),
+        },
+        _ => Ok(0),
+    }
+}
+
+fn clear_event_stream() -> Result<()> {
+    while poll(Duration::from_millis(1))? {
+        read()?;
+    }
+
+    Ok(())
 }
