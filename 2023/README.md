3eda6760ae861defdcad8c3cdae6ec58483968a4
Eric Frees <eric.frees@hexagon.com>
2023-12-02 11:34:06 -0600

    Add 2023 solution with initial day 1 solutions

---
+++ 2023/README.md
@@ -0,0 +1,47 @@
+# Advent of Code 2023
+Solutions to [Advent of Code 2023](https://adventofcode.com/2023)
+
+Yeah, I never did get around to doing 2022 (yet), but I'm tinkering with these again in 2023. .NET and C# have come a
+long way, and though most of the big changes won't impact these solutions at all, I may look to force some of the newer
+syntaxes in for the experience.
+
+I may end up solving some in other languages later and as has become the pattern, I'll track which solutions I've done,
+using the chart below.
+
+## Progress
+
+|      |1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|
+|------|-|-|-|-|-|-|-|-|-|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|
+|**C#**| | | | | | | | | |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
+
+⭐ - First star completed\
+🌟 - Both stars completed
+
+## Results
+I've added `output.txt` as a reference for my answers (for convenient checking while refactoring) and runtimes of my solutions.
+
+C# - [output.txt](csharp/output.txt)
+
+![Average runtimes](RuntimesChart.png)
+
+### Updating results
+To generate new output txt file
+```
+dotnet run --project .\AdventOfCode2023\AdventOfCode2023.csproj  --configuration=Release > output.txt
+```
+
+To update chart
+1. Generate new runtimes:
+```
+dotnet run --project .\AdventOfCode2023\AdventOfCode2023.csproj --configuration=AverageRuntimes
+```
+2. Copy the last lines into Excel file
+3. Save chart to image as "RuntimesChart.png"
+
+## Environment
+* Visual Studio 2022
+* [.NET 8](https://dotnet.microsoft.com/download/dotnet/8.0) / C# 12
+
+## Links
+* [Advent of Code](https://adventofcode.com)
+* [2021 Solutions](https://github.com/efrees/AdventOfCode2023)
---
+++ 2023/csharp/AdventOfCode2023/Solvers/Day01Solver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Solvers;
+
+internal class Day01Solver : ISolver
+{
+    private const string Name = "Day 1";
+    private const string InputFile = "day01input.txt";
+
+    public void Solve()
+    {
+        Console.WriteLine(Name);
+        var lines = Input.GetLinesFromFile(InputFile).ToList();
+
+        Console.WriteLine($"Output (part 1): {GetPart1Answer(lines)}");
+        Console.WriteLine($"Output (part 2): {GetPart2Answer(lines)}");
+    }
+
+    private static long GetPart1Answer(List<string> lines)
+    {
+        var firstDigits = lines.Select(line => line.First(char.IsDigit));
+        var lastDigits = lines.Select(line => line.Last(char.IsDigit));
+
+        return firstDigits
+            .Zip(lastDigits, (first, last) => Convert.ToInt32($"{first}{last}"))
+            .Sum();
+    }
+
+    private static long GetPart2Answer(List<string> lines)
+    {
+        // Just getting the whole list of matches with one Regex misses the overlapping ones
+        var firstDigitPattern = new Regex("[1-9]|one|two|three|four|five|six|seven|eight|nine");
+        var lastDigitPattern = new Regex("[1-9]|one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft);
+
+        var firstDigits = lines.Select(line => firstDigitPattern.Match(line).Value).Select(GetDigit);
+        var lastDigits = lines.Select(line => lastDigitPattern.Match(line).Value).Select(GetDigit);
+
+        return firstDigits
+            .Zip(lastDigits, (first, last) => Convert.ToInt32($"{first}{last}"))
+            .Sum();
+    }
+
+    private static long GetDigit(string digit)
+    {
+        return digit switch
+        {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            var other => Convert.ToInt32(other)
+        };
+    }
+}
