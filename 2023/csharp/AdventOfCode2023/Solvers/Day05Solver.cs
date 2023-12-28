08af837beb02580eedbe9296019cd4f859931c78
Eric Frees <eric.frees@hexagon.com>
2023-12-28 17:34:58 -0600

    Solve 2023, day 5, part 2
    This one was a mind-bender for me, with the main stumbles coming from
    1. Greedy processing, causing me to skip overlapping output ranges and
    2. Correctly identifying ranges only mapped in one of the two mappers
    being combined, since these have to be compared in a different transformation

--- 2023/csharp/AdventOfCode2023/Solvers/Day05Solver.cs
+++ 2023/csharp/AdventOfCode2023/Solvers/Day05Solver.cs
@@ -30,9 +30,36 @@ internal class Day05Solver : ISolver
 
     private static long GetPart2Answer(List<string> lines)
     {
-        //Naive solution requires processing billions of seeds; I'm mulling dynamic programming but skipping for now.
-        var seeds = ParseSeedsAsRanges(lines.First());
+        var seedRanges = ParseSeedsAsRanges(lines.First()).ToList();
         var maps = ParseRangeMaps(lines);
+        var reverseLookup = maps.Values.ToDictionary(map => map.ToType);
+
+        var currentMap = reverseLookup["location"];
+
+        while (currentMap.FromType != "seed")
+        {
+            var previous = reverseLookup[currentMap.FromType];
+
+            currentMap = FlattenRangeMaps(previous, currentMap);
+        }
+
+        var lowestLocationRanges = currentMap.Ranges.OrderBy(r => r.sourceMin + r.shift);
+
+        // Assumes (naively) that the lowest value will actually come from a mapped point, not an unmapped.
+
+        foreach (var mappedRange in lowestLocationRanges)
+        {
+            var overlappingSeedRanges = seedRanges
+                .Select(seedRange => (
+                    start: Math.Max(seedRange.start, mappedRange.sourceMin),
+                    end: Math.Min(seedRange.end, mappedRange.sourceMax)
+                ))
+                .Where(overlap => overlap.start < overlap.end);
+            if (overlappingSeedRanges.Any())
+            {
+                return overlappingSeedRanges.First().start + mappedRange.shift;
+            }
+        }
 
         return -1;
     }
@@ -60,7 +87,7 @@ internal class Day05Solver : ISolver
             .Select(long.Parse);
     }
 
-    private static IEnumerable<long> ParseSeedsAsRanges(string seedsLine)
+    private static IEnumerable<(long start, long end)> ParseSeedsAsRanges(string seedsLine)
     {
         var seedNumbers = seedsLine.Substring("seeds: ".Length)
             .Split(' ')
@@ -69,10 +96,8 @@ internal class Day05Solver : ISolver
 
         for (var i = 0; i < seedNumbers.Count; i += 2)
         {
-            for (var seedOffset = 0; seedOffset < seedNumbers[i + 1]; seedOffset++)
-            {
-                yield return seedNumbers[i] + seedOffset;
-            }
+            var seedCount = seedNumbers[i + 1];
+            yield return (seedNumbers[i], seedNumbers[i] + seedCount);
         }
     }
 
@@ -107,6 +132,75 @@ internal class Day05Solver : ISolver
         return maps;
     }
 
+    private static RangeMapper FlattenRangeMaps(RangeMapper leftMapper, RangeMapper rightMapper)
+    {
+        //Flatten the two mappers by splitting ranges
+        var flattenedMap = new RangeMapper(leftMapper.FromType, rightMapper.ToType);
+        var orderedIntermediateRanges = rightMapper.Ranges.OrderBy(r => r.sourceMin).ToList();
+
+        foreach (var left in leftMapper.Ranges)
+        {
+            var leftDestination = (min: left.sourceMin + left.shift, max: left.sourceMax + left.shift);
+
+            var overlappingIntermediates = orderedIntermediateRanges
+                .Where(r => r.sourceMax > leftDestination.min && r.sourceMin < leftDestination.max).ToList();
+            foreach (var right in overlappingIntermediates)
+            {
+                if (leftDestination.min < right.sourceMin)
+                {
+                    flattenedMap.Ranges.Add((leftDestination.min - left.shift, right.sourceMin - left.shift, left.shift));
+
+                    var maxOverlap = Math.Min(leftDestination.max, right.sourceMax);
+                    flattenedMap.Ranges.Add((right.sourceMin - left.shift, maxOverlap - left.shift, left.shift + right.shift));
+
+                    leftDestination = leftDestination with { min = maxOverlap };
+                }
+                else if (right.sourceMin <= leftDestination.min)
+                {
+                    var maxOverlap = Math.Min(leftDestination.max, right.sourceMax);
+                    flattenedMap.Ranges.Add((leftDestination.min - left.shift, maxOverlap - left.shift, left.shift + right.shift));
+
+                    leftDestination = leftDestination with { min = maxOverlap };
+                }
+            }
+
+            //Add any remaining part of the left range, since it doesn't overlap with any range in the right mapper
+            if (leftDestination.min < leftDestination.max)
+            {
+                flattenedMap.Ranges.Add((leftDestination.min - left.shift, left.sourceMax, left.shift));
+            }
+        }
+
+        var orderedLeftRanges = leftMapper.Ranges.OrderBy(l => l.sourceMin).ToList();
+        foreach (var right in rightMapper.Ranges)
+        {
+            var remainderOfRight = (min: right.sourceMin, max: right.sourceMax);
+            var overlappingLeftRanges = orderedLeftRanges.Where(l => l.sourceMax > right.sourceMin && l.sourceMin < right.sourceMax);
+            foreach (var left in overlappingLeftRanges)
+            {
+                if (remainderOfRight.min < left.sourceMin)
+                {
+                    flattenedMap.Ranges.Add((remainderOfRight.min, left.sourceMin, right.shift)); //example: 4,5
+                }
+
+                remainderOfRight = remainderOfRight with { min = left.sourceMax };
+            }
+
+            //Add any remaining part of the range, since it doesn't overlap with any source range already mapped
+            if (remainderOfRight.min < remainderOfRight.max)
+            {
+                flattenedMap.Ranges.Add((remainderOfRight.min, remainderOfRight.max, right.shift));
+            }
+        }
+
+        return flattenedMap;
+    }
+
+    private static bool IsEmptyRange((long sourceMin, long sourceMax, long shift) range)
+    {
+        return range.sourceMax <= range.sourceMin;
+    }
+
     private class RangeMapper(string fromType, string toType)
     {
         public string FromType { get; set; } = fromType;
@@ -116,7 +210,7 @@ internal class Day05Solver : ISolver
 
         public long Map(long inputValue)
         {
-            var matchingRange = Ranges.FirstOrDefault(range => inputValue >= range.sourceMin && inputValue <= range.sourceMax);
+            var matchingRange = Ranges.FirstOrDefault(range => inputValue >= range.sourceMin && inputValue < range.sourceMax);
 
             return matchingRange != default
                 ? inputValue + matchingRange.shift
