190e5f1880cbecc2e4b0e56edb8636ff3a89c525
Eric Frees <eric.frees@hexagon.com>
2024-01-01 21:26:49 -0600

    Simplify and optimize 2023, day 18

--- 2023/csharp/AdventOfCode2023/Solvers/Day18Solver.cs
+++ 2023/csharp/AdventOfCode2023/Solvers/Day18Solver.cs
@@ -1,5 +1,4 @@
-﻿using AdventOfCode2023.Grid;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -22,54 +21,14 @@ internal class Day18Solver : ISolver
 
     private static long GetPart1Answer(List<string> lines)
     {
-        var digSite = new SparseGrid<char>();
-        var potentialInsidePoints = new HashSet<Point2D>();
-
-        var currentPoint = Point2D.Origin;
-
-        foreach (var line in lines)
-        {
-            var splits = line.Split(' ');
-            var direction = splits[0][0];
-            var distance = int.Parse(splits[1]);
-            var color = splits[2];
-
-            foreach (var _ in Enumerable.Range(0, distance))
+        var digInstructions = lines.Select(line => line.Split(' '))
+            .Select(splits =>
             {
-                digSite.SetCell(currentPoint, '#');
-
-                //Assumes first trench cuts to the right.
-                var rightPoint = direction switch
-                {
-                    'U' => currentPoint.GetEastNeighbor(),
-                    'D' => currentPoint.GetWestNeighbor(),
-                    'L' => currentPoint.GetNorthNeighbor(),
-                    'R' => currentPoint.GetSouthNeighbor()
-                };
-                potentialInsidePoints.Add(rightPoint);
-
-                currentPoint = direction switch
-                {
-                    'U' => currentPoint.GetNorthNeighbor(),
-                    'D' => currentPoint.GetSouthNeighbor(),
-                    'L' => currentPoint.GetWestNeighbor(),
-                    'R' => currentPoint.GetEastNeighbor()
-                };
-            }
-        }
-
-        foreach (var inside in potentialInsidePoints)
-        {
-            if (digSite.GetCell(inside) == '#')
-            {
-                continue;
-            }
-
-            FloodFill(digSite, '#', inside);
-        }
-
-        var dugPointCount = digSite.GetAllCoordinates().Count();
-        return dugPointCount;
+                var direction = splits[0][0];
+                var distance = int.Parse(splits[1]);
+                return (direction, distance);
+            });
+        return ComputeEnclosedArea(digInstructions);
     }
 
     private static long GetPart2Answer(List<string> lines)
@@ -77,59 +36,46 @@ internal class Day18Solver : ISolver
         var digInstructions = lines
             .Select(line => line.Split(' ')[2])
             .Select(colorString => colorString[2..^1])
-            .Select(hexDigits => (direction: hexDigits[5] - '0', distance: int.Parse(hexDigits[..5], NumberStyles.HexNumber)));
+            .Select(hexDigits => (
+                direction: hexDigits[5] switch
+                {
+                    '0' => 'R',
+                    '1' => 'D',
+                    '2' => 'L',
+                    '3' => 'U',
+                    _ => throw new ArgumentOutOfRangeException()
+                },
+                distance: int.Parse(hexDigits[..5], NumberStyles.HexNumber)
+            ));
 
         return ComputeEnclosedArea(digInstructions);
     }
 
-    private static long ComputeEnclosedArea(IEnumerable<(int direction, int distance)> digInstructions)
+    private static long ComputeEnclosedArea(IEnumerable<(char direction, int distance)> digInstructions)
     {
         var totalArea = 1L;
         var currentCutDepth = 1L;
 
-        foreach (var instruction in digInstructions)
+        foreach (var (direction, distance) in digInstructions)
         {
-            if (instruction.direction == 0)
+            switch (direction)
             {
-                currentCutDepth += instruction.distance;
-                totalArea += instruction.distance;
-            }
-            else if (instruction.direction == 1)
-            {
-                totalArea += currentCutDepth * instruction.distance;
-            }
-            else if (instruction.direction == 2)
-            {
-                currentCutDepth -= instruction.distance;
-            }
-            else if (instruction.direction == 3)
-            {
-                totalArea -= (currentCutDepth - 1) * instruction.distance;
+                case 'R':
+                    currentCutDepth += distance;
+                    totalArea += distance;
+                    break;
+                case 'D':
+                    totalArea += currentCutDepth * distance;
+                    break;
+                case 'L':
+                    currentCutDepth -= distance;
+                    break;
+                case 'U':
+                    totalArea -= (currentCutDepth - 1) * distance;
+                    break;
             }
         }
 
         return totalArea;
-    }
-
-    private static void FloodFill(SparseGrid<char> digSite, char fillChar, Point2D seed)
-    {
-        var frontier = new Queue<Point2D>();
-        frontier.Enqueue(seed);
-        while (frontier.Any())
-        {
-            var fillPoint = frontier.Dequeue();
-
-            if (digSite.GetCell(fillPoint) == fillChar)
-            {
-                continue;
-            }
-
-            digSite.SetCell(fillPoint, fillChar);
-
-            foreach (var neighbor in fillPoint.GetNeighbors4())
-            {
-                frontier.Enqueue(neighbor);
-            }
-        }
     }
 }
