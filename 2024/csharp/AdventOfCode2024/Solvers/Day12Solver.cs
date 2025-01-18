aed2e56d5be01cf7831d5144539941cd811f101d
Eric Frees <eric.frees@hexagon.com>
2025-01-18 16:46:13 -0600

    Solve 2024, day 12, part 2

--- 2024/csharp/AdventOfCode2024/Solvers/Day12Solver.cs
+++ 2024/csharp/AdventOfCode2024/Solvers/Day12Solver.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode2024.Solvers;
 internal class Day12Solver : ISolver
 {
     private const string Name = "Day 12";
-    private const string InputFile = "day12input_sample.txt";
+    private const string InputFile = "day12input.txt";
 
     public void Solve()
     {
@@ -24,17 +24,19 @@ internal class Day12Solver : ISolver
     {
         var grid = SparseGrid<char>.Parse(lines, c => c);
 
-        var visited = new HashSet<Point2D>();
+        var visited = new SparseGrid<int>();
+        var nextRegionNumber = 1;
 
         var regions = new List<(int Area, int Perimeter)>();
         foreach (var cell in grid.GetAllCells())
         {
-            if (visited.Contains(cell.Coordinates))
+            if (visited.GetCell(cell.Coordinates) > 0)
             {
                 continue;
             }
 
-            regions.Add(ComputeAreaAndPerimeter(cell.Coordinates, cell.Value, grid, visited));
+            regions.Add(ComputeAreaAndPerimeter(cell.Coordinates, cell.Value, grid, visited, nextRegionNumber));
+            nextRegionNumber++;
         }
 
         return regions
@@ -45,9 +47,10 @@ internal class Day12Solver : ISolver
     private static (int Area, int Perimeter) ComputeAreaAndPerimeter(Point2D cellCoordinates,
         char cellValue,
         SparseGrid<char> grid,
-        HashSet<Point2D> visited)
+        SparseGrid<int> visited,
+        int regionNumber)
     {
-        visited.Add(cellCoordinates);
+        visited.SetCell(cellCoordinates, regionNumber);
 
         var area = 1;
         var perimeter = 0;
@@ -57,9 +60,9 @@ internal class Day12Solver : ISolver
             {
                 perimeter++;
             }
-            else if (!visited.Contains(neighbor))
+            else if (visited.GetCell(neighbor) == 0)
             {
-                var nestedResult = ComputeAreaAndPerimeter(neighbor, cellValue, grid, visited);
+                var nestedResult = ComputeAreaAndPerimeter(neighbor, cellValue, grid, visited, regionNumber);
                 area += nestedResult.Area;
                 perimeter += nestedResult.Perimeter;
             }
@@ -72,48 +75,145 @@ internal class Day12Solver : ISolver
     {
         var grid = SparseGrid<char>.Parse(lines, c => c);
 
-        var visited = new HashSet<Point2D>();
+        var visited = new SparseGrid<int>();
+        var nextRegionNumber = 1;
 
-        var regions = new List<(int Area, int Sides)>();
+        var regions = new Dictionary<int, Region>();
+
         foreach (var cell in grid.GetAllCells())
         {
-            if (visited.Contains(cell.Coordinates))
+            if (visited.GetCell(cell.Coordinates) > 0)
             {
                 continue;
             }
 
-            var areaAndPerimeter = ComputeAreaAndPerimeter(cell.Coordinates, cell.Value, grid, visited);
-            var sides = WalkPerimeterToCountSides(cell.Coordinates, cell.Value, grid);
-            regions.Add((areaAndPerimeter.Area, sides));
+            var areaAndPerimeter = ComputeAreaAndPerimeter(cell.Coordinates, cell.Value, grid, visited, nextRegionNumber);
+
+            var region = new Region
+            {
+                Id = nextRegionNumber,
+                StartCoordinates = cell.Coordinates,
+                Area = areaAndPerimeter.Area
+            };
+            regions[nextRegionNumber] = region;
+            nextRegionNumber++;
         }
 
-        // > 812010
+        foreach (var regionId in regions.Keys)
+        {
+            var region = regions[regionId];
+            var (sides, outerNeighbors) = WalkPerimeterToCountSides(
+                region.StartCoordinates,
+                visited,
+                location => visited.GetCell(location) != regionId
+            );
+            region.OuterSides = sides;
+            region.OuterNeighbors = outerNeighbors;
+        }
+
+        var innerNeighborsLookup = MapInnerNeighbors(regions);
+        var emptyList = new List<int>();
+        foreach (var region in regions.Values)
+        {
+            var innerNeighbors = innerNeighborsLookup.GetValueOrDefault(region.Id, emptyList);
+            if (!innerNeighbors.Any())
+            {
+                continue;
+            }
+
+            // To count inner sides, find all the fully-enclosed regions, and walk the sides as a cluster.
+            // Add additional regions to the cluster until we have only sides bordering our outer region.
+            // Surely there's a smarter way to do this.
+            var innerCluster = new List<int>();
+            do
+            {
+                if (innerCluster.Count == 0)
+                {
+                    innerCluster.Add(innerNeighbors.First());
+                }
+
+                var startCoordinates = innerCluster.Select(id => regions[id].StartCoordinates)
+                    .OrderBy(point => point.Y)
+                    .ThenBy(point => point.X)
+                    .First();
+                var (sides, outerNeighbors) = WalkPerimeterToCountSides(
+                    startCoordinates,
+                    visited,
+                    location => !innerCluster.Contains(visited.GetCell(location))
+                );
+
+                if (outerNeighbors.Count == 1)
+                {
+                    region.InnerSides += sides;
+                    innerNeighbors.RemoveAll(n => innerCluster.Contains(n));
+                    innerCluster = [];
+                }
+                else
+                {
+                    innerCluster.AddRange(outerNeighbors.Where(n => n != region.Id));
+                }
+            } while (innerNeighbors.Any());
+        }
+
         return regions
-            .Select(r => r.Area * r.Sides)
+            .Values
+            .Select(r => r.Area * (r.OuterSides + r.InnerSides))
             .Sum();
     }
 
-    private static int WalkPerimeterToCountSides(Point2D cellCoordinates, char cellValue, SparseGrid<char> grid)
+    private static Dictionary<int, List<int>> MapInnerNeighbors(Dictionary<int, Region> regions)
     {
-        var sides = new SparseGrid<char>();
+        var innerNeighbors = new Dictionary<int, List<int>>();
+
+        foreach (var region in regions.Values)
+        {
+            // A region is an inner neighbor of all its outer neighbors that don't have it as an outer neighbor.
+            foreach (var neighborId in region.OuterNeighbors)
+            {
+                var neighbor = regions[neighborId];
+                if (!neighbor.OuterNeighbors.Contains(region.Id))
+                {
+                    if (!innerNeighbors.ContainsKey(neighborId))
+                    {
+                        innerNeighbors[neighborId] = new List<int>();
+                    }
+
+                    innerNeighbors[neighborId].Add(region.Id);
+                }
+            }
+        }
+
+        return innerNeighbors;
+    }
+
+    private static (int Sides, HashSet<int> OuterNeighbors) WalkPerimeterToCountSides(Point2D startCoordinates,
+        SparseGrid<int> visited,
+        Func<Point2D, bool> checkIsOutsideRegion)
+    {
         // We encounter each region from the top (minY) side and from the left, so we start walking right.
         var currentSideDirection = new Point2D(1, 0);
-        var currentLocation = cellCoordinates;
-        sides.SetCell(currentLocation, '#');
+        var currentLocation = startCoordinates;
 
-        var sideCount = 1;
-        while (currentLocation != cellCoordinates || currentSideDirection != (0, -1))
+        var sideCount = 0;
+        var outerNeighbors = new HashSet<int>();
+        while (currentLocation != startCoordinates || currentSideDirection != (1, 0) || sideCount == 0)
         {
+            var leftHandCell = LeftHandCell(currentLocation, currentSideDirection);
+            if (checkIsOutsideRegion(leftHandCell) && visited.GetCell(leftHandCell) is var knownRegion and > 0)
+            {
+                outerNeighbors.Add(knownRegion);
+            }
+
             var nextLocation = currentLocation.Add(currentSideDirection);
 
-            if (grid.GetCell(nextLocation) != cellValue)
+            if (checkIsOutsideRegion(nextLocation))
             {
                 //...?
                 //AAA.
                 currentSideDirection = TurnRight(currentSideDirection);
                 sideCount++;
             }
-            else if (grid.GetCell(LeftHandCell(nextLocation, currentSideDirection)) == cellValue)
+            else if (!checkIsOutsideRegion(LeftHandCell(nextLocation, currentSideDirection)))
             {
                 //...A
                 //AAAA
@@ -125,16 +225,9 @@ internal class Day12Solver : ISolver
             {
                 currentLocation = nextLocation;
             }
-
-            sides.SetCell(currentLocation, '#');
         }
 
-        Console.WriteLine($"{cellValue}: {sideCount}");
-        Console.WriteLine(sides.RenderAsString(c => c == '\0'
-            ? '.'
-            : c));
-
-        return sideCount;
+        return (sideCount, outerNeighbors);
     }
 
     private static Point2D LeftHandCell(Point2D nextLocation, Point2D currentSideDirection)
@@ -151,5 +244,15 @@ internal class Day12Solver : ISolver
     private static (long Y, long) TurnRight(Point2D currentSideDirection)
     {
         return (-currentSideDirection.Y, currentSideDirection.X);
+    }
+
+    private class Region
+    {
+        public int Id { get; init; }
+        public Point2D StartCoordinates { get; init; }
+        public int Area { get; set; }
+        public int OuterSides { get; set; }
+        public int InnerSides { get; set; }
+        public HashSet<int> OuterNeighbors { get; set; }
     }
 }
